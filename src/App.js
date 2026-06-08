734fe5c1f86eec588c3dd97104ba562ef354ec07
tianshizhang <steventsz@outlook.com>
2026-06-08 12:21:04 +1200

    Set up basic routing, navigation, and API base URL

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router";
 import Navigation from "./components/Navigation";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Home() {
+    return (
+        <div>
+            <h1>Piki Ora 7420</h1>
+            <p>Welcome to the Piki Ora appointment booking frontend.</p>
+        </div>
+    );
+}
+
+function Doctors() {
+    return (
+        <div>
+            <h1>Doctors</h1>
+            <p>Doctor list page will be added here.</p>
+        </div>
+    );
+}
+
+function Slots() {
+    return (
+        <div>
+            <h1>Appointment Slots</h1>
+            <p>Available appointment slots will be added here.</p>
+        </div>
+    );
+}
+
+function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <p>Login form will be added here.</p>
+        </div>
+    );
+}
 
 function App() {
     return (
         <div className="App">
-          <Navigation/>
             <BrowserRouter>
+                <Navigation/>
                 <Routes>
-                    <Route path="/" element={<App/>}/>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/doctors" element={<Doctors/>}/>
+                    <Route path="/slots" element={<Slots/>}/>
+                    <Route path="/login" element={<Login/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,23 @@
-import React, {Fragment} from 'react';
-import {Nav} from "react-bootstrap";
+import React from 'react';
+import {Link} from "react-router";
+import {Container, Nav, Navbar} from "react-bootstrap";
 
 function Navigation(props) {
     return (
-        <Fragment>
-            <Nav variant="pills" defaultActiveKey="/home">
-                <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="disabled" disabled>
-                        Disabled
-                    </Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </Fragment>
+        <Navbar bg="light" expand="lg" className="mb-4">
+            <Container>
+                <Navbar.Brand as={Link} to="/">Piki Ora 7420</Navbar.Brand>
+                <Navbar.Toggle aria-controls="main-navigation"/>
+                <Navbar.Collapse id="main-navigation">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/doctors">Doctors</Nav.Link>
+                        <Nav.Link as={Link} to="/slots">Slots</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
